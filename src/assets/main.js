2e710c75e51df4bf9f3dc10aeb22adeadff7a675
katie <katya24081992@mail.ru>
2017-08-19 16:34:25 +0300

    fixed

--- src/assets/main.js
+++ src/assets/main.js
@@ -19,12 +19,12 @@ function guess() {
     	setMessage("You Win! :)");
     	showAnswer(true);
     	showReplay();
-    } else if (attempt.value > 10) {
+    } else if (attempt.value >= 10) {
     	setMessage("You Lose! :(");
     	showAnswer(false);
     	showReplay();
     } else {
-    	setMessage("Incorect, try again.");
+    	setMessage("Incorrect, try again.");
     }
 }
 
